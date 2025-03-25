a8c70de43d2b38922963a9d8cd40fbb838acd301
level237 <Bramslevel129@gmail.com>
2025-03-25 23:05:35 +0100

    Add index method to ReservationController: Implemented a method to retrieve and display user-specific reservations, enhancing the customer experience by showing active bookings in the 'my-reservations' view.

--- app/Http/Controllers/customer/ReservationController.php
+++ app/Http/Controllers/customer/ReservationController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers\customer;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class ReservationController extends Controller
 {
-    //
+    public function index()
+    {
+        $reservations=DB::table('rooms')
+                        ->join('reservations','rooms.id','reservations.room_id')
+                        ->join('hotels','hotels.id','rooms.hotel_id')
+                        
+                        ->select('reservations.id','reservations.check_in','reservations.check_out','reservations.status','reservations.price_reser','reservations.user_id','hotels.hotel_name','hotels.star_number','hotels.hotel_town','reservations.room_id','rooms.image')
+                        ->where('reservations.user_id',auth()->user()->id)
+                        ->where('reservations.status',1)
+                        ->get();
+        return view('customer.my-reservations',compact('reservations'));
+    }
 }
