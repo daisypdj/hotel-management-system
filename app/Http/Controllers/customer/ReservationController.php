f64c51a518ccb1f333a3f2328ad43cdc0b766a6f
level237 <Bramslevel129@gmail.com>
2025-03-25 23:20:10 +0100

    Add cancel and fastStore methods to ReservationController: Implemented functionality to cancel reservations and store fast reservation details, improving the booking process and user experience in the application.

--- app/Http/Controllers/customer/ReservationController.php
+++ app/Http/Controllers/customer/ReservationController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\customer;
 
-use App\Models\Room_type;
+use Carbon\Carbon;
 use App\Models\Hotel;
+use App\Models\Room_type;
+use App\Models\Reservation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
@@ -29,4 +31,34 @@ class ReservationController extends Controller
         $classes=Room_Type::all();
         return view('customer.reservation-fast',compact('hotels','classes'));
     }
+
+    public function cancel($id){
+
+        $reservation=Reservation::find($id);
+        $reservation->update([
+            'status'=>0,
+        ]);
+
+
+        return redirect()->back()->with('cancel','reservation annulée');
+    }
+
+     public function fastStore(Request $request){
+
+        $classe=$request->classe;
+        $rooms=DB::table('rooms')
+                ->join('room__types','rooms.room__type_id','room__types.id')
+                ->select('rooms.id','rooms.hotel_id','rooms.image','room__types.title','room__types.price','room__types.adult_capacity','room__types.kids_capacity')
+                ->where('room__types.title',$classe)
+                ->where('rooms.hotel_id',$request->hotel)
+                ->get();
+        
+        $begin=Carbon::parse($request->checkIn);
+        $end=Carbon::parse($request->checkOut);
+        $lenght=$end->diffInDays($begin);
+        $request->session()->put('duree',$lenght);
+        $request->session()->put('checkIn',$request->checkIn);
+        $request->session()->put('checkOut',$request->checkOut);
+        return view('Hotel.step-two',compact('rooms'));
+    }   
 }
