44d26adf09c4e74f0d31c25ba598749fd4139f52
level237 <Bramslevel129@gmail.com>
2025-03-28 17:52:18 +0100

    Add fillable attributes to Reservation model: Defined the fillable properties for the Reservation model to streamline mass assignment and improve data handling.

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     use HasFactory;
+    protected $fillable=['room_id','check_in','check_out','duration_of_stay','total_price','user_id','status'];
 }
